6653a93f020313f7237bac015ca62b6c386c4cfd
bonissanti <brunoonissanti@gmail.com>
2025-01-08 14:44:28 -0300

    feat: editing config.js and manage.py

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -1,1 +1,4 @@
-fetch()
+fetch('http://backend:8000/api/v1/resources')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error adding resources', error))
