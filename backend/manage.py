0b25fb6b25985fbdbdad482f05de08bbeb6d6f45
Bruno Onissanti <brunoonissanti@gmail.com>
2025-01-25 18:39:50 -0300

    feat: database adjust + health check fixed
    environment variable (database) fixed, health check from backend and
    front commente, and from database fixed

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main() -> object:
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
