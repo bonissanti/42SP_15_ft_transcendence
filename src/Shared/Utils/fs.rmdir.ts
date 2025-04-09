9ae9e84a10692677a24b260f55f2825ad1eb26f6
Bruno Onissanti <brunoonissanti@gmail.com>
2025-04-08 22:00:10 -0300

    feat: Add utility script to remove the 'dist' directory
    This script checks if the 'dist' directory exists and deletes it recursively if present. It logs success or error messages accordingly, ensuring better control over build artifact cleanup.

---
+++ src/Shared/Utils/fs.rmdir.ts
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const distPath = path.resolve(__dirname, '../../dist');
+
+if (fs.existsSync(distPath))
+{
+    fs.rm(distPath, { recursive: true }, (err: null) => {
+        if (err)
+            console.error('Error while deleting directory ${distPath}', err)
+        else
+            console.log('Directory ${distPath} deleted successfully')
+    });
+}
+else
+    console.log(`Directory ${distPath} does not exists`);
