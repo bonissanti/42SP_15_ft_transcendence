fe025663475aa380e6b149c0ed7ecf00a5af51f8
Bruno Onissanti <brunoonissanti@gmail.com>
2025-04-12 20:01:33 -0300

    chore: deletado projeto django
    Remove unnecessary files, templates, and configurations
    
    This commit removes unused files such as `.gitignore`, PyCharm configurations, stylesheets, JavaScript configs, and HTML templates. It also deletes redundant scripts and data dumps to clean up and streamline the repository.

--- backend/user_auth/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from dotenv import load_dotenv
-
-load_dotenv()
-class LoginlogicConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_auth'
--- frontend/static/js/config.js
+++
@@ -1,4 +0,0 @@
-fetch('http://backend:8000/api/v1/resources')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log('Error adding resources', error))
