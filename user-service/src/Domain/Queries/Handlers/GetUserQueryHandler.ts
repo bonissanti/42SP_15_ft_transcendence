288fa3ad4a36bc882a58236ffc054bf3d151964d
Bruno Onissanti <brunoonissanti@gmail.com>
2025-07-19 20:49:20 -0300

    refactor: reorganize and update imports in `GetUserService` and related components
    - Moved `DTO`, `Validators`, and `Query` files to appropriate `Application` and `Domain` subdirectories.
    - Updated import paths across affected services, handlers, and view models.

--- user-service/src/Domain/Queries/Handlers/GetUserQueryHandler.ts
+++ user-service/src/Domain/Queries/Handlers/GetUserQueryHandler.ts
@@ -1,9 +1,9 @@
 import {GetUserQuery} from "../QueryObject/GetUserQuery.js";
 import {UserRepository} from "../../../Infrastructure/Persistence/Repositories/Concrete/UserRepository.js";
 import {NotificationError} from "../../../Shared/Errors/NotificationError.js";
-import {GetUserDTO} from "../../../Domain/DTO/Query/GetUserDTO.js";
+import {GetUserDTO} from "../../../Application/DTO/ToQuery/GetUserDTO.js";
 import {BaseHandlerQuery} from "./BaseHandlerQuery.js";
-import {GetUserQueryDTO} from "../../../Domain/QueryDTO/GetUserQueryDTO.js";
+import {GetUserQueryDTO} from "../../QueryDTO/GetUserQueryDTO.js";
 
 export class GetUserQueryHandler implements BaseHandlerQuery<GetUserQuery | null>
 {
