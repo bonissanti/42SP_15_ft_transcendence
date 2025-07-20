7c90b1cde09857f75bb68c911b9b722d98d029e1
Bruno Onissanti <brunoonissanti@gmail.com>
2025-07-20 18:10:16 -0300

    feat: add token generation utility and improve coding style consistency
    - Added `generateToken` function in `AuthMiddleware` for JWT creation with a 1-hour expiration.
    - Enhanced code formatting across `AuthMiddleware` and `BaseController` for better readability.
    - Adjusted imports and structure of handler files during package reorganization.

--- user-service/src/Presentation/Middleware/AuthMiddleware.ts
+++ user-service/src/Presentation/Middleware/AuthMiddleware.ts
@@ -9,3 +9,9 @@ export const authenticateJWT = async (request: any, reply: any): Promise<void> =
         reply.status(401).send({ message: '' });
     }
 }
+
+const jwt_secret = 'my-jwt';
+
+const generateToken = (userId: number): string => {
+    return jwt.sign({ userId }, jwt_secret, { expiresIn: '1h' });
+}
