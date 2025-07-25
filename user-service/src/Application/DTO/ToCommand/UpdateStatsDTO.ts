7bd23445562e1be68badbef48459ab9c3578de49
bruno-onissanti <bruno.onissanti@viceri.com.br>
2025-07-25 18:58:35 -0300

    Refactor DTOs to use new enum names and update UpdateStatsDTO
    Replaced StatusRequest with StatusRequestEnum in AddRequestFriendDTO and ChangeRequestFriendStatusDTO for consistency with updated enum naming. Updated UpdateStatsDTO to include gameType (GameTypeEnum), reordered constructor parameters, and adjusted property assignments to match the new signature.

--- user-service/src/Application/DTO/ToCommand/UpdateStatsDTO.ts
+++ user-service/src/Application/DTO/ToCommand/UpdateStatsDTO.ts
@@ -1,24 +1,28 @@
+import {GameTypeEnum} from "../../Enums/GameTypeEnum.js";
+
 export class UpdateStatsDTO
 {
+    public readonly gameType: GameTypeEnum
     public readonly player1Username: string;
-    public readonly player1Points: number;
     public readonly player2Username: string;
-    public readonly player2Points: number;
     public readonly player3Username: string | null;
-    public readonly player3Points: number | null;
     public readonly player4Username: string | null;
+    public readonly player1Points: number;
+    public readonly player2Points: number;
+    public readonly player3Points: number | null;
     public readonly player4Points: number | null;
 
-    constructor(player1Username: string, player1Points: number, player2Username: string, player2Points: number,
-                player3Username: string | null, player3Points: number | null, player4Username: string | null, player4Points: number | null)
+    constructor(gameType: GameTypeEnum, player1Username: string, player2Username: string, player3Username: string | null,
+                player4Username: string | null, player1Points: number, player2Points: number, player3Points: number | null, player4Points: number | null)
     {
+        this.gameType = gameType;
         this.player1Username = player1Username;
-        this.player1Points = player1Points;
         this.player2Username = player2Username;
-        this.player2Points = player2Points;
         this.player3Username = player3Username;
-        this.player3Points = player3Points;
         this.player4Username = player4Username;
+        this.player1Points = player1Points;
+        this.player2Points = player2Points;
+        this.player3Points = player3Points;
         this.player4Points = player4Points;
     }
 }
