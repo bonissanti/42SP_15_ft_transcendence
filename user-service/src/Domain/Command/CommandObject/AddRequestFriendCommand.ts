9a159a7e3af855f1cb00e7582b56a58967a667f9
bruno-onissanti <bruno.onissanti@viceri.com.br>
2025-07-25 18:59:33 -0300

    Refactor: enums and extend UpdateStatsCommand for more players
    Replaced StatusRequest with StatusRequestEnum across relevant files for consistency. Updated UpdateStatsCommand to support up to four players and include game type, allowing for more flexible game statistics handling.

--- user-service/src/Domain/Command/CommandObject/AddRequestFriendCommand.ts
+++ user-service/src/Domain/Command/CommandObject/AddRequestFriendCommand.ts
@@ -1,13 +1,13 @@
 import {AddRequestFriendDTO} from "../../../Application/DTO/ToCommand/AddRequestFriendDTO.js";
-import {StatusRequest} from "../../../Application/Enums/StatusRequest.js";
+import {StatusRequestEnum} from "../../../Application/Enums/StatusRequestEnum.js";
 
 export class AddRequestFriendCommand
 {
-    public readonly status: StatusRequest;
+    public readonly status: StatusRequestEnum;
     public readonly receiverUuid: string;
     public readonly senderUuid: string;
 
-    constructor (status: StatusRequest, receiverUuid: string, senderUuid: string)
+    constructor (status: StatusRequestEnum, receiverUuid: string, senderUuid: string)
     {
         this.status = status;
         this.receiverUuid = receiverUuid;
