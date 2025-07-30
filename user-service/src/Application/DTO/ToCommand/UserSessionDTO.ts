fd3f657a3408b60809346db6e3589353518eed6e
Bruno Onissanti <brunoonissanti@gmail.com>
2025-07-30 19:18:00 -0300

    refactor: remove uuid property from UserSessionDTO and UserSessionCommand
    - Eliminated `uuid` property in `UserSessionDTO` and `UserSessionCommand`.
    - Updated constructors and static mapping methods to exclude `uuid`.

--- user-service/src/Application/DTO/ToCommand/UserSessionDTO.ts
+++ user-service/src/Application/DTO/ToCommand/UserSessionDTO.ts
@@ -1,14 +1,12 @@
 export class UserSessionDTO
 {
-    public readonly uuid: string;
     public readonly email: string;
     public readonly password: string;
     public readonly lastLogin: Date;
     public readonly isOnline: boolean;
 
-    constructor(uuid: string, email: string, password: string, lastlogin: Date, isOnline: boolean)
+    constructor(email: string, password: string, lastlogin: Date, isOnline: boolean)
     {
-        this.uuid = uuid;
         this.email = email;
         this.password = password;
         this.lastLogin = lastlogin;
--- user-service/src/Domain/Command/CommandObject/UserSessionCommand.ts
+++ user-service/src/Domain/Command/CommandObject/UserSessionCommand.ts
@@ -2,14 +2,12 @@ import {UserSessionDTO} from "../../../Application/DTO/ToCommand/UserSessionDTO.
 
 export class UserSessionCommand
 {
-    public readonly Uuid: string;
     public readonly Email: string;
     public readonly Password: string;
     public readonly isOnline: boolean;
 
-    constructor(uuid: string, email: string, password: string, isOnline: boolean)
+    constructor(email: string, password: string, isOnline: boolean)
     {
-        this.Uuid = uuid;
         this.Email = email;
         this.Password = password;
         this.isOnline = isOnline;
@@ -17,6 +15,6 @@ export class UserSessionCommand
 
     public static FromDTO(dto: UserSessionDTO): UserSessionCommand
     {
-        return new UserSessionCommand(dto.uuid, dto.email, dto.password, dto.isOnline);
+        return new UserSessionCommand(dto.email, dto.password, dto.isOnline);
     }
 }
